371f182ebef10a4d8c4c2c7cd42277fad5c5dd16
shaque <shaque@omers.com>
2021-10-16 02:58:06 -0400

    complete code

---
+++ pages/api/graphql.js
@@ -0,0 +1,60 @@
+import Cors from 'micro-cors'
+import { gql, ApolloServer } from 'apollo-server-micro'
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin'
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster'
+    }
+]
+
+const typeDefs = gql`
+    type Book {
+        title: String
+        author: String
+    }
+
+    type Query {
+        books: [Book]
+    }
+`
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+};
+
+const cors = Cors()
+
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+
+    },
+    introspection: true,
+    playground: true,
+});
+
+const serversStart = apolloServer.start();
+
+export default cors(async(req, res) => {
+    if(req.method === 'OPTIONS') {
+        res.end();
+        return false;
+    }
+
+    await serversStart;
+    await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+});
